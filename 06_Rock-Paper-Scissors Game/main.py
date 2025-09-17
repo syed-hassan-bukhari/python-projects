71c79766082666ade5cf6a6ba40695c3b0327e13
syed-hassan-bukhari <hassankhann900@gmail.com>
2025-09-18 01:22:24 +0500

    initial commit

--- 06_Rock-Paper-Scissors Game/main.py
+++ 06_Rock-Paper-Scissors Game/main.py
@@ -4,14 +4,13 @@ user_score = 0
 com_score = 0
 tie = 0
 
-# Ask for choice 
-print("\nChoices are: \n1. Rock \n2. Paper \n3. Scissor")
-choice = int(input("Enter your choice from 1-3: "))
-while choice > 3 or choice < 1:
-choice = int(input("Enter a valid input (1-3): "))
-
 while True:
-    # User choice
+    print("\nChoices are: \n1. Rock \n2. Paper \n3. Scissor")
+    choice = int(input("Enter your choice from 1-3: "))
+    while choice > 3 or choice < 1:
+        choice = int(input("Enter a valid input (1-3): "))
+
+    
     if choice == 1:
         user_choice = "rock"
     elif choice == 2:
@@ -20,12 +19,12 @@ while True:
         user_choice = "scissor"
     print("You chose:", user_choice)
 
-    # Computer choice
+    
     computer = random.randint(1, 3)
     com_choice = ["rock", "paper", "scissor"][computer - 1]
     print("Computer chose:", com_choice)
 
-    # Decide winner
+    
     if ((user_choice == "paper" and com_choice == "rock") or
         (user_choice == "rock" and com_choice == "paper")):
         print("Paper wins")
@@ -41,24 +40,24 @@ while True:
         print("Scissor wins")
         result = "scissor"
 
-    # Update scores
+    
     if result == "tie":
         tie += 1
     elif result == user_choice:
-        print("You win this round!")
+        print(" You win this round!")
         user_score += 1
     else:
         print("Computer wins this round!")
         com_score += 1
 
-    # Show scores
+    
     print(f"Scores → You: {user_score} | Computer: {com_score} | Ties: {tie}")
 
-    # Ask to play again
+    
     repeat = input("Do you want to play again? (Y/N): ")
     if repeat.lower() == "n":
         break
-    # If 'y' → continue automatically because of while True
+    
 
 print("GAME OVER")
 print(f"Final Score → You: {user_score}, Computer: {com_score}, Ties: {tie}")
---
+++ 07_Word Counter/main.py
@@ -0,0 +1,18 @@
+import os
+
+def count_words(file_path):
+    try:
+        
+        file_path = os.path.expanduser(file_path)
+
+        with open(file_path, "r", encoding="utf-8") as file:
+            text = file.read()
+            words = text.split()
+            print(f"\n✅ Total words in '{file_path}': {len(words)}")
+    except FileNotFoundError:
+        print("\n File not found. Please check the path and try again.")
+    except Exception as e:
+        print(f"\n An error occurred: {e}")
+
+file_path = input("Enter full file path : ").strip()
+count_words(file_path)
